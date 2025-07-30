7bc0791f97f8a28db9443a00f6e582dac58d0333
TetianaPotochna <t.potochna@gmail.com>
2025-07-30 15:48:03 +0300

    HomeWork2

--- src/test/java/HomeWork.java
+++ src/test/java/HomeWork.java
@@ -12,28 +12,73 @@ public class HomeWork {
     WebDriver wd;
 
     @BeforeClass
-    public void setUp() {
+    public void SetUp() {
         wd = new ChromeDriver();
         wd.get("https://telranedu.web.app/");
     }
 
     @Test
-    public void ccsSelectors() {
+    public void cssSelectors() {
         //by tagName
         WebElement html = wd.findElement(By.tagName("html"));
+        WebElement html1 = wd.findElement(By.cssSelector("html"));
+        WebElement html2 = wd.findElement(By.xpath("html"));
+
         WebElement body = wd.findElement(By.tagName("body"));
+        WebElement body1 = wd.findElement(By.cssSelector("body"));
+        WebElement body2 = wd.findElement(By.xpath("//body"));
+
         WebElement div = wd.findElement(By.tagName("div"));
-        List<WebElement> listDiv = wd.findElements(By.tagName("div"));
+        WebElement div1 = wd.findElement(By.cssSelector("div"));
+        WebElement div2 = wd.findElement(By.xpath("//div"));
+
         WebElement h1 = wd.findElement(By.tagName("h1"));
+        WebElement h1a = wd.findElement(By.cssSelector("h1"));
+        WebElement h1b = wd.findElement(By.xpath("//h1"));
+
         WebElement a = wd.findElement(By.tagName("a"));
-        List<WebElement> list_a = wd.findElements(By.tagName("a"));
-        // WebElement elClass = wd.findElement(By.tagName("class")); - test failed. Why?
-        List<WebElement> list_class = wd.findElements(By.tagName("class"));
-        List<WebElement> form = wd.findElements(By.tagName("form"));
+        WebElement a1 = wd.findElement(By.cssSelector("a"));
+        WebElement a2 = wd.findElement(By.xpath("//a"));
+
+        List<WebElement> listDiv = wd.findElements(By.tagName("div"));
+        List<WebElement> listDiv1 = wd.findElements(By.cssSelector("div"));
+        List<WebElement> listDiv2 = wd.findElements(By.xpath("//div"));
+
+        List<WebElement> listA = wd.findElements(By.tagName("a"));
+        List<WebElement> listA1 = wd.findElements(By.cssSelector("a"));
+        List<WebElement> listA2 = wd.findElements(By.xpath("//a"));
+
+        //by class
+        WebElement byClass = wd.findElement(By.className("container"));
+        WebElement byClass1 = wd.findElement(By.cssSelector(".container"));
+        WebElement byClass2 = wd.findElement(By.xpath("//*[@class = 'container']"));
+
+        WebElement divByClass = wd.findElement(By.className("navbar-component_nav__1X_4m"));
+        WebElement divByClass1 = wd.findElement(By.cssSelector(".navbar-component_nav__1X_4m"));
+        WebElement divByClass2 = wd.findElement(By.xpath("//*[@class = 'navbar-component_nav__1X_4m']"));
+
+        WebElement a_ByClass = wd.findElement(By.className("active"));
+        WebElement a_ByClass1 = wd.findElement(By.cssSelector(".active"));
+        WebElement a_ByClass2 = wd.findElement(By.xpath("//*[@class = 'active']"));
+
+        //by id
+        WebElement root = wd.findElement(By.id("root"));
+        WebElement root1 = wd.findElement(By.cssSelector("#root"));
+        WebElement root2 = wd.findElement(By.xpath("//*[@id = 'root']"));
+
+        //By attribute
+        WebElement home = wd.findElement(By.cssSelector("[href='/home']"));
+        WebElement home1 = wd.findElement(By.xpath("//*[@href = '/home']"));
+
+        WebElement page = wd.findElement(By.cssSelector("[aria-current = 'page']"));
+        WebElement page1 = wd.findElement(By.xpath("//*[@aria-current = 'page']"));
+
+        //WebElement registration = wd.findElement(By.name("registration"));
+        //WebElement registration1 = wd.findElement(By.cssSelector("[name = 'registration']"));
     }
 
     @AfterClass
-    public void postCondition() {
+    public void SetDown() {
         wd.quit();
     }
 }
--- src/test/java/Index.java
+++ src/test/java/Index.java
@@ -2,6 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -12,16 +13,66 @@ public class Index {
     WebDriver wd;
 
     @BeforeClass
-    public void setUp(){
+    public void setUp() {
         wd = new ChromeDriver();
         wd.get("file:///C:/Users/User/Downloads/21.index.html");
     }
 
     @Test
-    public void cssLocators(){
+    public void tableTest(){
+        //Canada
+        WebElement canada = wd.findElement(By.cssSelector("tr:nth-child(3)>td:last-child"));
+        Assert.assertEquals(canada.getText(), "Canada");
+    }
+
+
+    @Test
+    public void cssLocators() {
         //by tag name
-        WebElement el =  wd.findElement(By.tagName("button"));
+        WebElement el = wd.findElement(By.tagName("button"));
+        WebElement el1 = wd.findElement(By.xpath("//button"));
+
         WebElement input = wd.findElement(By.tagName("input"));
+        WebElement input2 = wd.findElement(By.cssSelector("input"));
+        WebElement input3 = wd.findElement(By.xpath("//input"));
+
         List<WebElement> list = wd.findElements(By.tagName("input"));
+
+        //by class
+        WebElement div = wd.findElement(By.className("container"));
+        WebElement div1 = wd.findElement(By.cssSelector(".container"));
+        WebElement div2 = wd.findElement((By.xpath("//*[@class = 'container']")));
+
+        List<WebElement> List = wd.findElements(By.className("nav-item"));
+        List<WebElement> List1 = wd.findElements(By.cssSelector(".nav-item"));
+        List<WebElement> list2 = wd.findElements(By.xpath("//*[@class = 'nav-item']"));
+
+        //by id
+        WebElement nav = wd.findElement(By.id("nav"));
+        WebElement nav1 = wd.findElement(By.cssSelector("#nav"));
+        WebElement nav2 = wd.findElement(By.xpath("//*[@id = 'nav']"));
+
+        //By attribute
+        WebElement inputEmail = wd.findElement(By.cssSelector("[placeholder='Type your name']"));
+        WebElement a3 = wd.findElement(By.cssSelector("[href='#item3']"));
+        WebElement inputEmail4 = wd.findElement(By.xpath("//*[@placeholder='Type your name']"));
+
+        //start
+        WebElement inputEmail1 = wd.findElement(By.cssSelector("[placeholder^='Type']"));
+
+        //end
+        WebElement inputEmail2 = wd.findElement(By.cssSelector("[placeholder$='name']"));
+        //contains
+        WebElement inputEmail3 = wd.findElement(By.cssSelector("[placeholder*='your']"));
+
+
+        //by name
+        WebElement inputS = wd.findElement(By.name("surename"));
+        WebElement inputS1 = wd.findElement(By.cssSelector("[name='surename']"));
+
+        //By.linkText / By.partialLinkText
+        WebElement a = wd.findElement(By.linkText("Item 3"));
+        WebElement a2 = wd.findElement(By.partialLinkText("m 2"));
+
     }
 }
